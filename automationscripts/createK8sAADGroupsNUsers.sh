0794fe7c5cd967497d924bacb98526e5cfe00128
Sriram Ganesan <gsriramit@gmail.com>
2022-01-26 20:47:01 +0530

    issue fixes

--- automationscripts/createK8sAADGroupsNUsers.sh
+++ automationscripts/createK8sAADGroupsNUsers.sh
@@ -11,10 +11,12 @@ export TENANTID_AZURERBAC_AKS_BASELINE=$(az account show --query tenantId -o tsv
 export TENANTID_K8SRBAC_AKS_BASELINE=$(az account show --query tenantId -o tsv)
 
 # Create the Cluster Admin AAD group and export the object Id for future use. 
-export AADOBJECTID_GROUP_CLUSTERADMIN_AKS_BASELINE= $(az ad group create --display-name 'cluster-admins-bu0001a000800' --mail-nickname 'cluster-admins-bu0001a000800' --description "Principals in this group are cluster admins in the bu0001a000800 cluster." --force false --query objectId -o tsv)
+export AADOBJECTID_GROUP_CLUSTERADMIN_AKS_BASELINE=$(az ad group create --display-name 'cluster-admins-bu0001a000800' --mail-nickname 'cluster-admins-bu0001a000800' --description "Principals in this group are cluster admins in the bu0001a000800 cluster." --force false --query objectId -o tsv)
 
 # Create a "break-glass" cluster administrator user for your AKS cluster.
-TENANTDOMAIN_K8SRBAC=$(az ad signed-in-user show --query 'userPrincipalName' -o tsv | cut -d '@' -f 2 | sed 's/\"//')
+#object-id of any domain user- this will be used to fetch the domain suffix
+domainuserObjectId='d5d5599b-0ddd-46b0-9d54-8f0a04f4b5e6'
+TENANTDOMAIN_K8SRBAC=$(az ad user show --id  $domainuserObjectId --query 'userPrincipalName' -o tsv | cut -d '@' -f 2 | sed 's/\"//')
 AADOBJECTNAME_USER_CLUSTERADMIN=bu0001a000800-admin
 AADOBJECTID_USER_CLUSTERADMIN=$(az ad user create --display-name=${AADOBJECTNAME_USER_CLUSTERADMIN} --user-principal-name ${AADOBJECTNAME_USER_CLUSTERADMIN}@${TENANTDOMAIN_K8SRBAC} --force-change-password-next-login --password ChangeMebu0001a0008AdminChangeMe --query objectId -o tsv)
 
