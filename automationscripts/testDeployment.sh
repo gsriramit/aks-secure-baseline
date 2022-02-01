54db18454fc16d7c625095557133a0af55fe6418
Sriram Ganesan <gsriramit@gmail.com>
2022-02-01 15:29:15 +0530

    Test deployment

---
+++ automationscripts/testDeployment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Cluster Parameters
+RGNAMECLUSTER=$1
+RGNAMESPOKES=$2
+TENANT_ID=$3
+SP_OBJECTID= $4
+
+echo "Current working directory: $(pwd)"
+echo "Value of Tenant Id:  $TENANT_ID"
+echo "Value of SP Object Id: $SP_OBJECTID"
+
+cd ../cluster-manifests/cluster-baseline-settings
+echo "Current working directory after change: $(pwd)"
